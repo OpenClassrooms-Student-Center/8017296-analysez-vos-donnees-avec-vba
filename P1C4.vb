0b0a37fab5e7bf5eea02b77d560444e83d9f068e
JeremyOllier <148545927+JeremyOllier@users.noreply.github.com>
2023-11-08 23:11:04 +0100

    Update P1C4.vb

--- P1C4.vb
+++ P1C4.vb
@@ -16,36 +16,36 @@ If nom_utilisteur = "Jeremy" Then
     
     jour = Date
         
-    'ouverture du fichier
+    'Ouverture du fichier
     Workbooks.Open Filename:="d:\C1P3\reporting.xlsb"
         
-    'recherche du jour
+    'Recherche du jour
     Sheets("Reporting").Select
     Range("D3:D40").Select
     Selection.Find(What:=jour, After:=ActiveCell, LookIn:=xlFormulas _
             , LookAt:=xlPart, SearchOrder:=xlByRows, SearchDirection:=xlNext, _
             MatchCase:=False, SearchFormat:=False).Activate
     
-    'récupération des parametrages des cellules
+    'Récupération des paramétrages des cellules
     num_ligne = ActiveCell.Row
     statut = Range("E" & num_ligne).Value
     etat = Range("G" & num_ligne).Value
     
-    'teste si on doit lancer le sous programme
+    'Teste si on doit lancer le sous-programme
     If (statut = "Oui" And etat = "") Then
         Liste_fichier
         Range("G" & num_ligne).Select
         ActiveCell.FormulaR1C1 = "Fait"
     End If
     
-    'créer une phrase a envoyer par email
+    'Créer une phrase à envoyer par email
     If Range("G" & num_ligne).Value = "Fait" Then
-        Resultat = "La mise à jour est passé correctement en date du : " & jour
+        Resultat = "la mise à jour est passée correctement en date du : " & jour
     Else
-        Resultat = "Probleme avec la mise à jour du : " & jour
+        Resultat = "Problème avec la mise à jour du : " & jour
     End If
     
-    'Création et envoie de l'email automatiquement
+    'Création et envoi de l'email automatiquement
     With outlook.Createitem(alMailitem)
         .Subject = "Reporting - " & jour
         .To = "jeremy.ollier@gmail.com"
