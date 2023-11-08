f53de1631feac0ff15ffed9169d2355513c07f0f
JeremyOllier <148545927+JeremyOllier@users.noreply.github.com>
2023-11-08 23:07:53 +0100

    Update P1C3S1.vb

--- P1C3S1.vb
+++ P1C3S1.vb
@@ -7,22 +7,22 @@ Dim num_ligne As Integer
 
 jour = Date
     
-'ouverture du fichier
+'Ouverture du fichier
 'Workbooks.Open Filename:="D:\C1P3\reporting.xlsb"
     
-'recherche du jour
+'Recherche du jour
 Sheets("Reporting").Select
 Range("D3:D40").Select
 Selection.Find(What:=jour, After:=ActiveCell, LookIn:=xlFormulas _
         , LookAt:=xlPart, SearchOrder:=xlByRows, SearchDirection:=xlNext, _
         MatchCase:=False, SearchFormat:=False).Activate
 
-'récupération des parametrages des cellules
+'Récupération des paramétrages des cellules
 num_ligne = ActiveCell.Row
 statut = Range("E" & num_ligne).Value
 etat = Range("G" & num_ligne).Value
 
-'test de lancement du sous programme
+'Test de lancement du sous-programme
 If (statut = "Oui" And etat = "") Then
     Liste_fichiers
     Range("G" & num_ligne).Select
